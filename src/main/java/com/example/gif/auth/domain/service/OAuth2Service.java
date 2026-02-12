59d88a36e22e1c538eb3e3dc3cb406581c122ed1
user <sooooooobinn@gmail.com>
2026-02-12 19:33:01 +0900

    feat: service 코드 작성

---
+++ src/main/java/com/example/gif/auth/domain/service/OAuth2Service.java
@@ -0,0 +1,74 @@
+package com.example.gif.auth.domain.service;
+
+import com.example.gif.auth.domain.dto.UserProfile;
+import com.example.gif.auth.domain.entity.User;
+import com.example.gif.auth.domain.repository.UserRepository;
+import com.example.gif.auth.global.security.oauth.OAuthAttributes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+@RequiredArgsConstructor
+public class OAuth2Service implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService oAuth2UserService = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        String userNameAttributeName = userRequest
+                .getClientRegistration()
+                .getProviderDetails()
+                .getUserInfoEndpoint()
+                .getUserNameAttributeName();
+
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        UserProfile userProfile = OAuthAttributes.extract(registrationId, attributes);
+        userProfile.setProvider(registrationId);
+
+        updateOrSaveUser(userProfile);
+
+        Map<String, Object> customAttribute = getCustomAttribute(registrationId, userNameAttributeName, attributes, userPRofile);
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority("USER")),
+                customAttribute,
+                userNameAttributeName);
+    }
+
+    public Map getCustomAttribute(String registrationId, String userNameAttributeName, Map<String, Object> attributes, UserProfile userProfile) {
+        Map<String, Object> customAttribute = new ConcurrentHashMap<>();
+
+        customAttribute.put(userNameAttributeName, attributes.get(userNameAttributeName));
+        customAttribute.put("provider", registrationId);
+        customAttribute.put("name", userProfile.getUsername());
+        customAttribute.put("email", userProfile.getEmail());
+
+        return customAttribute;
+    }
+
+    public User updateOrSaveUser(UserProfile userProfile) {
+        User user = userRepository
+                .findUserByEmailAndProvider(userProfile.getEmail(), userProfile.getProvider())
+                .map(value -> value.updateUSer(userProfile.getUsername(), userProfile.getProvider()))
+                .orElse(userProfile.toEntity());
+
+        return userRepository.save(user);
+    }
+}

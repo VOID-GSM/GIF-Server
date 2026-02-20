130cecd7730a0d98f7eb4bf5ae62a26e20960c6b
user <sooooooobinn@gmail.com>
2026-02-21 02:25:42 +0900

    feat: client dto 수정

--- src/main/java/com/example/gif/auth/domain/dto/ClientAdditionalInfo.java
+++ src/main/java/com/example/gif/auth/domain/dto/ClientAdditionalInfo.java
@@ -8,14 +8,4 @@ public class ClientAdditionalInfo {
     private String username;
     private String studentNumber;
     private User.Role role;
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public void setStudentNumber(String studentNumber) {
-        this.studentNumber = studentNumber;
-    }
-    public void setRole(User.Role role) {
-        this.role = role;
-    }
 }

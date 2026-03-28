c4b97793e0d134dd6dc2e61b7ff6e556833ce089
hikeong <s25048@gsm.hs.kr>
2026-03-29 02:15:05 +0900

    chore: merge 충돌 해결

--- src/main/java/com/example/gif/domain/form/entity/FieldStyle.java
+++
@@ -1,7 +0,0 @@
-package com.example.gif.domain.form.entity;
-
-public enum FieldStyle {
-    CALENDAR,
-    TEXT,
-    FILE
-}
--- src/main/java/com/example/gif/domain/form/repository/FormRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gif.domain.form.repository;
-
-import com.example.gif.domain.form.entity.Form;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FormRepository extends JpaRepository<Form, Long> {
-}

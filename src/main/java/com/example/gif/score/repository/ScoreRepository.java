d002aefb1b409a88f8a513e4495e754b3620cb4c
user <sooooooobinn@gmail.com>
2026-03-31 00:51:53 +0900

    feat: Repository findByTeamName 추가

--- src/main/java/com/example/gif/score/repository/ScoreRepository.java
+++ src/main/java/com/example/gif/score/repository/ScoreRepository.java
@@ -1,7 +1,11 @@
 package com.example.gif.score.repository;
 
+import com.example.gif.project.entity.Project;
 import org.springframework.data.domain.Score;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ScoreRepository extends JpaRepository<Score, Long> {
+    Optional<Score> findByTeamName(Project teamName);
 }
